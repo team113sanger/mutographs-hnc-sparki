f709cb3fe4b91c7fec44b4eb8fb6bc10c4f252f7
Jacqueline Marcia Boccacino <jb62@sanger.ac.uk>
2025-10-14 13:28:26 +0100

    Re-organised files in results/

--- scripts/collate_mapping_stats_pre_filtering.sh
+++ scripts/collate_mapping_stats_pre_filtering.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "all_count,unmapped_count,mapped_count" > results/collated_mapping_stats_pre_filtering.csv
-cat results/fastq/*.csv | grep -v "all_count" >> results/collated_mapping_stats_pre_filtering.csv
+echo "all_count,unmapped_count,mapped_count" > results/sparki-nf/collated_mapping_stats_pre_filtering.csv
+cat results/fastq/*.csv | grep -v "all_count" >> results/sparki-nf/collated_mapping_stats_pre_filtering.csv
